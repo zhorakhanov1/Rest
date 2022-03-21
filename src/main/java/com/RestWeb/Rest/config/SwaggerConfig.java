351e220881ee53c40ed8cec034f06d4f09440f21
Alisher <zhorakhanov1@mail.ru>
2022-03-21 16:14:51 +0600

    Initial commit

--- src/main/java/com/RestWeb/Rest/config/SwaggerConfig.java
+++ src/main/java/com/RestWeb/Rest/config/SwaggerConfig.java
@@ -15,8 +15,8 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.RestWeb.Rest.Controller"))
+                .paths(PathSelectors.ant("/api/*"))
                 .build();
     }
 }
